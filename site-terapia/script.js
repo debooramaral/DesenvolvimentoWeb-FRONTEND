a4fbc15f4b2cce3ef4787819766e58356af36428
Débora Amaral <87581131+debooramaral@users.noreply.github.com>
2022-08-19 21:40:44 -0400

    corrigido
    ajuda do professor.. parece qe ele sabe até

--- site-terapia/script.js
+++ site-terapia/script.js
@@ -1,18 +1,26 @@
 // ALERTA SIMPLES
 
-function funcao1(){
+/*function funcao1(){
     alert("ALERTAAAAAAAAAA !")
-}
+}*/
 
 
 // FUNÇÃO MELHORADA
 
-/*function funcao1(){
+/*function insiradados(){
     var email = document.getElementById("email").value;
-    if(email == null || nome == ""){
+    if(email == null || email == ""){
         alert("Faça o favor de colocar o EMAIL ")
     } else{
         alert("O email: " + email);
+    }
+}
+function insiradadoss(){
+    var password = document.getElementById("password").value;
+    if(password == null || email == ""){
+        alert("Cuidado, precisa de SENHA")
+    } else{
+        alert("OTIMO :D");
     }
 }*/
 
@@ -20,9 +28,15 @@ function funcao1(){
 //VALIDAR CAMPOS DO FORMULÁRIO
 
 /*function valida_form(){
-    if(document.getElementById("email".value.length < 10)){
+    if(document.getElementById("email").value.length < 10){
         alert("Coloque seu E-Mail, poxa ;x")
     }
     document.getElementById("email").focus();
+
+
+    if(document.getElementById("password").value.length < 3){
+        alert("Coloque a Senha, poxa ;x")
+    }
+    document.getElementById("password").focus();
     return false
 }*/
