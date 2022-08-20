cf2d1682a89e96e7e0f00154b5daaa39169f8b16
Débora Amaral <87581131+debooramaral@users.noreply.github.com>
2022-08-19 21:16:01 -0400

    CSS e JS
    ajustei o posicionamento dos elementos, pois o formulário não pegava o haver . . desenvolvendo alertas no JS (aula bosta, explicação péssima)

---
+++ site-terapia/script.js
@@ -0,0 +1,28 @@
+// ALERTA SIMPLES
+
+function funcao1(){
+    alert("ALERTAAAAAAAAAA !")
+}
+
+
+// FUNÇÃO MELHORADA
+
+/*function funcao1(){
+    var email = document.getElementById("email").value;
+    if(email == null || nome == ""){
+        alert("Faça o favor de colocar o EMAIL ")
+    } else{
+        alert("O email: " + email);
+    }
+}*/
+
+
+//VALIDAR CAMPOS DO FORMULÁRIO
+
+/*function valida_form(){
+    if(document.getElementById("email".value.length < 10)){
+        alert("Coloque seu E-Mail, poxa ;x")
+    }
+    document.getElementById("email").focus();
+    return false
+}*/
