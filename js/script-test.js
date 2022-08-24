8e2e0fa02fdcc0eec2eade236c22d6a297054237
Débora Amaral <87581131+debooramaral@users.noreply.github.com>
2022-08-23 21:14:17 -0400

    Add files via upload

--- js/script-test.js
+++ js/script-test.js
@@ -37,3 +37,68 @@ const RG = '123456';
 //console.log(RG)
 //alert(RG)
 
+const expr = 'Mamão';
+
+// switch(expr){
+//     case 'Laranja':
+//         console.log('Laranja é R$ 0.59 centavos');
+//         break;
+//     case 'Manga':
+//     case 'Mamão':
+//         console.log('Manga e Mamão custam R$ 2.79 reais');
+//         break;
+//     default:
+//         console.log('Desculpe, estamos sem ' + expr)
+// }
+
+var foo = 10;
+
+var output = 'Output: ';
+
+/*switch(foo){
+    case 10: 
+        output += 'Então ';
+    case 1:
+        output += 'Qual ';
+        output += 'É ';
+    case 2:
+        output += 'O Seu ';
+    case 3:
+        output += 'Nome ';
+    case 4:
+        output += '?';
+        alert(output);
+        break
+    case 5:
+        output += '!';
+        alert(output);
+        break
+    default:
+        alert('Favor escolher um número de 0 a 6 !');
+}*/
+
+var passo;
+
+/*for (passo = 0; passo < 5; passo ++){
+    //executa 5 vezes, com os valores de passos de 0 a 4.
+    console.log('Ande um passo para o leste ' + passo)
+}*/
+
+var i = 0
+
+/*do {
+    i += 1;
+    console.log(i);
+} while (i<5);*/
+
+n = 0;
+x = 0;
+
+while (n < 3){
+    n++; //acrescenta 1
+    console.log('esse é o n ' + n)
+    x += n; //valor que ja tem + valor do n
+    console.log('esse é o x ' + x)
+}
+
+
